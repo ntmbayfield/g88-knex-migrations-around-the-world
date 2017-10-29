4d9660673650ee61909f1e4aa7a1c503c2594347
Wes Reid <bwreid@gmail.com>
2017-10-29 14:43:38 -0700

    Include sequence reset on 03 seeds example

--- 03-seeds-example/db/seeds/countries.js
+++ 03-seeds-example/db/seeds/countries.js
@@ -1,12 +1,16 @@
-exports.seed = knex => {
+exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
   return knex('countries').del()
     .then(() => {
       // Inserts seed entries
       return knex('countries').insert([
-        { name: 'Germany', population: 82114224 },
-        { name: 'Puerto Rico', population: 3663131 },
-        { name: 'Qatar', population: 2639211 }
+        { id: 1, name: 'Germany', population: 82114224 },
+        { id: 2, name: 'Puerto Rico', population: 3663131 },
+        { id: 3, name: 'Qatar', population: 2639211 }
       ])
+    }).then(() => {
+      return knex.raw(
+        `SELECT setval('countries_id_seq', (SELECT MAX(id) FROM countries));`
+      );
     })
 }
