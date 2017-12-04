2c3b805a91c31511cdeb9fea92630786054449d8
rogerwschmidt <rogerschmidt@gmail.com>
2017-12-04 10:33:03 -0800

    updated scripts

--- 04-seed-tables/test.sh
+++ 04-seed-tables/test.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
+# must match the name of the service for the database in
+# the docker-compose.yml file
+DATABASE_SERVICE=db
+
+# check to see if script is running in docker
+if [ -f /.dockerenv ]; then
+    # get tool to check if database is ready
+    apt-get update && apt-get install -y postgresql-client
+
+    # spin until database is ready
+    while
+      pg_isready -h $DATABASE_SERVICE
+      [ $? -ne 0 ]
+    do :;  done
+fi
+
+
+# run test
 npm test
