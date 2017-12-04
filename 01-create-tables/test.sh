428c7848b01353f54b0f7b4e72186a5b2fe4de97
rogerwschmidt <rogerschmidt@gmail.com>
2017-12-04 13:28:07 -0800

    updating docker config

--- 01-create-tables/test.sh
+++ 01-create-tables/test.sh
@@ -4,24 +4,23 @@
 # the docker-compose.yml file
 DATABASE_SERVICE=db
 COUNTER=0
-EXITCODE=1
+EXIT_CODE=1
+SECONDS_BEFORE_TIMEOUT=59
 
 # check to see if script is running in docker
 if [ -f /.dockerenv ]; then
-    # get tool to check if database is ready
 
     # spin until database is ready
-
-    while [ $EXITCODE -ne 0 ]
+    while [ $EXIT_CODE -ne 0 ]
     do
-
       pg_isready -h $DATABASE_SERVICE
-      EXITCODE=$?
+      EXIT_CODE=$?
       COUNTER=`expr $COUNTER + 1`
 
       sleep 1
 
-      if [ $COUNTER -ge 59 ]; then
+      # Check for time out
+      if [ $COUNTER -ge $SECONDS_BEFORE_TIMEOUT ]; then
         echo "Timed Out - Database not Found"
         exit 1
       fi
