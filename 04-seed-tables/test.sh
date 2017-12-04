695b829419d300d22eea067f07f016025f0917ef
rogerwschmidt <rogerschmidt@gmail.com>
2017-12-04 13:40:56 -0800

    updating docker config

--- 04-seed-tables/test.sh
+++ 04-seed-tables/test.sh
@@ -3,19 +3,31 @@
 # must match the name of the service for the database in
 # the docker-compose.yml file
 DATABASE_SERVICE=db
+COUNTER=0
+EXIT_CODE=1
+SECONDS_BEFORE_TIMEOUT=59
 
 # check to see if script is running in docker
 if [ -f /.dockerenv ]; then
-    # get tool to check if database is ready
-    apt-get update && apt-get install -y postgresql-client
 
     # spin until database is ready
-    while
+    while [ $EXIT_CODE -ne 0 ]
+    do
       pg_isready -h $DATABASE_SERVICE
-      [ $? -ne 0 ]
-    do :;  done
+      EXIT_CODE=$?
+      COUNTER=`expr $COUNTER + 1`
+
+      sleep 1
+
+      # Check for time out
+      if [ $COUNTER -ge $SECONDS_BEFORE_TIMEOUT ]; then
+        echo "Timed Out - Database not Found"
+        exit 1
+      fi
+
+    done
 fi
 
 
-# run test
+
 npm test
