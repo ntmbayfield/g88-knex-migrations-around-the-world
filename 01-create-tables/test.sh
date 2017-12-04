7cffad6bbd82e41b11a3cc5d020ee78c3d53dacb
rogerwschmidt <rogerschmidt@gmail.com>
2017-12-04 13:11:44 -0800

    Trying out new tests

--- 01-create-tables/test.sh
+++ 01-create-tables/test.sh
@@ -3,19 +3,32 @@
 # must match the name of the service for the database in
 # the docker-compose.yml file
 DATABASE_SERVICE=db
+COUNTER=0
+EXITCODE=1
 
 # check to see if script is running in docker
 if [ -f /.dockerenv ]; then
     # get tool to check if database is ready
-    apt-get update && apt-get install -y postgresql-client
 
     # spin until database is ready
-    while
+
+    while [ $EXITCODE -ne 0 ]
+    do
+
       pg_isready -h $DATABASE_SERVICE
-      [ $? -ne 0 ]
-    do :;  done
+      EXITCODE=$?
+      COUNTER=`expr $COUNTER + 1`
+
+      sleep 1
+
+      if [ $COUNTER -ge 59 ]; then
+        echo "Timed Out - Database not Found"
+        exit 1
+      fi
+
+    done
 fi
 
 
-# run test
+
 npm test
