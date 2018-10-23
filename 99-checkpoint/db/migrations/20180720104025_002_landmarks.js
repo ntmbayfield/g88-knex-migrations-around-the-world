f60df0dce40147555b8ce69fb304bc2060d6d4df
Nyiema Bayfield <ntmbayfield@gmail.com>
2018-10-23 13:20:29 -0700

    All tests for tables are passing.

--- 99-checkpoint/db/migrations/20180720104025_002_landmarks.js
+++ 99-checkpoint/db/migrations/20180720104025_002_landmarks.js
@@ -5,8 +5,7 @@ exports.up = function(knex, Promise) {
     table.string('name').notNullable().defaultsTo('')
     table.integer('established').notNullable().defaultsTo(0)
     table.integer('city_id').notNullable().defaultsTo(0)
-    table.foreign('city_id').references('cities.id').onDelete('CASCADE')
-    table.timestamp(true, true)
+    table.timestamps(true, true)
   })
 };
 
